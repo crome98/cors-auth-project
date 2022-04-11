b8125b8c971351fe7388cee57dd0e21d5724281e
phuclh <mark15071995@gmail.com>
2022-04-11 22:25:02 +0900

    - 2022-04-11:         - Removing deprecated config when making connection to DB

--- config/database.js
+++ config/database.js
@@ -1,17 +1,12 @@
+// jshint esversion:6
+
 const mongoose = require("mongoose");
 
 const { MONGO_URI } = process.env;
 
 exports.connect = () => { // we can use connect function from other modules
 	// Connecting to the database
-	mongoose.connect(MONGO_URI, null
-		//{
-			//useNewUrlParser: true,
-			//useUnifiedTopology: true,
-			//useCreateIndex: true,
-			//useFindAndModify: false,
-	//}
-	)
+	mongoose.connect(MONGO_URI, null)
 	.then(() => {
 		console.log(" ^v^ Successfully, connected to DB");
 	})
