5fe2e21aa420ea8624cf8737e3df4b2d58d21857
phuclh <mark15071995@gmail.com>
2022-04-12 15:26:35 +0900

    - 2022-04-12: 	+ Switch DBMS from mongoose to mariadb

--- config/database.js
+++ config/database.js
@@ -1,18 +1,14 @@
 // jshint esversion:6
 
-const mongoose = require("mongoose");
+const mariadb = require("mariadb");
+require("dotenv").config();
 
-const { MONGO_URI } = process.env;
+const pool = mariadb.createPool({
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME,
+	connectionLimit: process.env.DB_CONNECTION_LIMIT
+});
 
-exports.connect = () => { // we can use connect function from other modules
-	// Connecting to the database
-	mongoose.connect(MONGO_URI, null)
-	.then(() => {
-		console.log(" ^v^ Successfully, connected to DB");
-	})
-	.catch(error => {
-		console.log("T_T can not connect to DB, exiting now... bye");
-		console.log(error);
-		process.exit(1);
-	});
-};
+exports.pool = pool;
